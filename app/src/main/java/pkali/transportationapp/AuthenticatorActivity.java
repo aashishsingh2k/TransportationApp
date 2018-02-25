cf9928c8289ee03c3686aa0684163115a329b7cd
aashishsingh2k <saashish@seas.upenn.edu>
2018-02-25 03:03:45 -0500

    commit 1

--- app/src/main/java/pkali/transportationapp/AuthenticatorActivity.java
+++ app/src/main/java/pkali/transportationapp/AuthenticatorActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AuthenticatorActivity extends AppCompatActivity {
-
+    //yoyooyoyoyo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
