323d406d29db880a044b13c97795784fe1fdbcd3
pkalicheti <pkalicheti@gmail.com>
2018-02-25 02:17:42 -0500

    test

---
+++ app/src/main/java/pkali/transportationapp/AuthenticatorActivity.java
@@ -0,0 +1,13 @@
+package pkali.transportationapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthenticatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticator);
+    }
+}
